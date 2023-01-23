605ae1706a2995d3a5222ca21be4156e509ca7cf
RetiredHedgehog <lukashov9999@gmail.com>
2023-01-24 00:35:18 +0300

    feat: add classes to tie together components

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,16 +1,17 @@
-import AppController from "components/controller/appController";
-import AppView from "components/view/appView";
+import AppController from '../controller/appController';
+import AppView from '../view/appView';
 
 export default class App {
   controller: AppController;
+
   view: AppView;
 
   constructor() {
-    this.controller = new AppController();
+    this.controller = new AppController(`http://127.0.0.1:3000`);
     this.view = new AppView();
   }
 
   start() {
-    return;
+    this.view.init(this.controller);
   }
 }
--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,3 +1,53 @@
-export default class AppView {
+import AppController from 'components/controller/appController';
+import fillCarsDisplay from './carsDisplay/fillCarsDisplay';
+import Controls from './controls/controls';
 
+import updateCarCounter from './updateCarCounter';
+
+export default class AppView {
+  root: HTMLElement;
+  controls: Controls;
+
+  constructor() {
+    this.root =
+      document.getElementsByTagName('body')[0] ||
+      document.appendChild(document.createElement('body'));
+    this.controls = new Controls();
+  }
+
+  init(controller: AppController) {
+    this.controls.drawInputs(controller, this.root);
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('garage-view');
+
+    const wrapperHeading = document.createElement('div');
+    wrapperHeading.classList.add('garage-view__heading');
+
+    const title = document.createElement('h2');
+    title.classList.add('garage-view__title');
+    title.innerText = 'Garage';
+
+    const amount = document.createElement('p');
+    amount.classList.add('garage-view__amount');
+    amount.innerText = '(...)';
+
+    wrapperHeading.append(title, amount);
+
+    const wrapperCarDisplay = document.createElement('div');
+    wrapperCarDisplay.classList.add('cars-display');
+
+    wrapper.append(wrapperHeading, wrapperCarDisplay);
+
+    const buttonGetCars = document.createElement('button');
+    buttonGetCars.classList.add('btn', 'btn-get-cars');
+    buttonGetCars.innerText = 'Get cars';
+    buttonGetCars.addEventListener('click', async () => {
+      await updateCarCounter(controller);
+    });
+
+    this.root.append(buttonGetCars, wrapper);
+    updateCarCounter(controller);
+    fillCarsDisplay(controller);
+  }
 }
